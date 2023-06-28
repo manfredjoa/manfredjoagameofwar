9d9b1ae945f30190eccb53c888287d1822d9ddab
manfredjoa <joamanfred@gmail.com>
2023-06-28 13:11:12 -0400

    add conditionals, add while loop, fixed while loop error

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// To do
-// Create function that plays the game
-// Create a pile array within this function from which cards played will be pushed into.
-
 //
 class Card {
   constructor(suit, rank, score) {
@@ -27,6 +23,7 @@ class Deck {
     for (let i = 0; i < suits.length; i++) {
 
       for (let j = 0; j < ranks.length; j++) {
+        // this.cards.push(new Card(suits[i], ranks[j], j + 2))
         this.cards.push(new Card(suits[i], ranks[j], j + 2))
       }
     }
@@ -63,7 +60,100 @@ const splitDeck = () => {
 }
 
 splitDeck()
-
-console.log(playerOne)
-console.log(playerTwo)
 //
+
+//
+let gameOfWar = () => {
+  let playerOneCard = []
+  let playerTwoCard = []
+  let tiedPile = []
+
+  while (playerOne.cards[0].length > 0 && playerTwo.cards[0].length > 0) {
+
+    playerOneCard.push(playerOne.cards[0].shift())
+    playerTwoCard.push(playerTwo.cards[0].shift())
+
+    if (playerOneCard[0].score > playerTwoCard[0].score) {
+      console.log(`${playerOne.name} played a ${playerOneCard[0].rank} and ${playerTwo.name} played a ${playerTwoCard[0].rank}. ${playerOne.name} wins!`)
+
+      playerOne.cards[0].push(playerOneCard[0])
+      playerOne.cards[0].push(playerTwoCard[0])
+      // playerOneCard = []
+      // playerTwoCard = []
+
+      console.log(`${playerOne.name}: ${playerOne.cards[0].length} cards`)
+      console.log(`${playerTwo.name}: ${playerTwo.cards[0].length} cards`)
+
+    } else if (playerTwoCard[0].score > playerOneCard[0].score) {
+      console.log(`${playerOne.name} played a ${playerOneCard[0].rank} and ${playerTwo.name} played a ${playerTwoCard[0].rank}. ${playerTwo.name} wins!`)
+
+      playerTwo.cards[0].push(playerOneCard[0])
+      playerTwo.cards[0].push(playerTwoCard[0])
+      // playerOneCard = []
+      // playerTwoCard = []
+
+      console.log(`${playerOne.name}: ${playerOne.cards[0].length} cards`)
+      console.log(`${playerTwo.name}: ${playerTwo.cards[0].length} cards`)
+
+    } else while (playerOneCard[0].score === playerTwoCard[0].score) {
+      if (playerOne.cards[0].length < 4) {
+        console.log(`${playerOne.name} does not have enough cards to go to War. ${playerTwo.name} wins the game!`)
+
+      } else if (playerTwo.cards[0].length < 4) {
+        console.log(`${playerTwo.name} does not have enough cards to go to War. ${playerOne.name} wins the game!`)
+      }
+
+      console.log(`${playerOne.name} played a ${playerOneCard[0].rank} and ${playerTwo.name} played a ${playerTwoCard[0].rank}. War!`)
+
+      tiedPile.push(playerOneCard.shift())
+      tiedPile.push(playerTwoCard.shift())
+
+      tiedPile.push(playerOne.cards[0].shift())
+      tiedPile.push(playerOne.cards[0].shift())
+      tiedPile.push(playerOne.cards[0].shift())
+
+      tiedPile.push(playerTwo.cards[0].shift())
+      tiedPile.push(playerTwo.cards[0].shift())
+      tiedPile.push(playerTwo.cards[0].shift())
+
+      playerOneCard.push(playerOne.cards[0].shift())
+      playerTwoCard.push(playerTwo.cards[0].shift())
+
+      if (playerOneCard[0].score > playerTwoCard[0].score) {
+        console.log(`${playerOne.name} played a ${playerOneCard[0].rank} and ${playerTwo.name} played a ${playerTwoCard[0].rank}. ${playerOne.name} wins the war!`)
+
+        playerOne.cards[0].push(playerOneCard[0])
+        playerOne.cards[0].push(playerTwoCard[0])
+        playerOne.cards[0].push(...tiedPile)
+
+        // playerOneCard = []
+        // playerTwoCard = []
+        tiedPile = []
+
+        console.log(`${playerOne.name}: ${playerOne.cards[0].length} cards`)
+        console.log(`${playerTwo.name}: ${playerTwo.cards[0].length} cards`)
+
+      } else if (playerTwoCard[0].score > playerOneCard[0].score) {
+        console.log(`${playerOne.name} played a ${playerOneCard[0].rank} and ${playerTwo.name} played a ${playerTwoCard[0].rank}. ${playerTwo.name} wins the war!`)
+
+        playerTwo.cards[0].push(playerOneCard[0])
+        playerTwo.cards[0].push(playerTwoCard[0])
+        playerTwo.cards[0].push(...tiedPile)
+
+        // playerOneCard = []
+        // playerTwoCard = []
+        tiedPile = []
+
+        console.log(`${playerOne.name}: ${playerOne.cards[0].length} cards`)
+        console.log(`${playerTwo.name}: ${playerTwo.cards[0].length} cards`)
+      }
+    }
+  } if (playerOne.cards[0].length === 0) {
+    console.log(`${playerOne.name} has no more cards. ${playerTwo.name} wins the game!`)
+
+  } else if (playerTwo.cards[0].length === 0) {
+    console.log(`${playerTwo.name} has no more cards. ${playerOne.name} wins the game!`)
+  }
+}
+
+console.log(gameOfWar())
