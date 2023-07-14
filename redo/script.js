b50759df3e57e72bfb3f44ac987c76f917146d54
manfredjoa <joamanfred@gmail.com>
2023-07-13 20:27:44 -0400

    fix function bugs

--- redo/script.js
+++ redo/script.js
@@ -1,4 +1,3 @@
-//
 class Card {
   constructor(suit, rank, score) {
     this.suit = suit
@@ -6,9 +5,8 @@ class Card {
     this.score = score
   }
 }
-//
 
-//
+
 class Deck {
   constructor() {
     this.cards = []
@@ -36,18 +34,16 @@ class Deck {
     }
   }
 }
-//
 
-//
+
 class Player {
   constructor(name) {
     this.name = name
     this.cards = []
   }
 }
-//
 
-// 
+
 const theDeck = new Deck()
 const playerOne = new Player('Player 1')
 const playerTwo = new Player('Player 2')
@@ -58,85 +54,87 @@ let playerOneDeck = playerOne.cards[0]
 let playerTwoDeck = playerTwo.cards[0]
 
 
-//
-
-//
 let gameOfWar = () => {
   let playerOneCard = []
   let playerTwoCard = []
-  let pile = []
+  let pileOne = []
+  let pileTwo = []
+  let combinedPile = []
 
   function playerOneWins() {
-    if (pile = []) {
-      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerOne.name} wins!`)
-
-    } else {
-      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerOne.name} wins the war!`)
-    }
+    console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerOne.name} wins!`)
     playerOneDeck.push(...playerOneCard); playerOneDeck.push(...playerTwoCard)
-    console.log(playerOneDeck.length, playerTwoDeck.length)
+    playerOneCard = []; playerTwoCard = []
+    console.log(`${playerOne.name}: ${playerOneDeck.length} cards, ${playerTwo.name}: ${playerTwoDeck.length} cards`)
   }
-  function playerTwoWins() {
-    if (pile = []) {
-      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerTwo.name} wins!`)
 
-    } else {
-      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerTwo.name} wins the war!`)
-    }
+  function playerTwoWins() {
+    console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. ${playerTwo.name} wins!`)
     playerTwoDeck.push(...playerOneCard); playerTwoDeck.push(...playerTwoCard)
-    console.log(playerOneDeck.length, playerTwoDeck.length)
+    playerOneCard = []; playerTwoCard = []
+    console.log(`${playerOne.name}: ${playerOneDeck.length} cards, ${playerTwo.name}: ${playerTwoDeck.length} cards`)
   }
+
   function war() {
     if (playerOneDeck.length < 4) {
-      console.log(`${playerOne.name} does not have enough cards to go to War. ${playerTwo.name} wins the game!`)
+      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. WAR!!!`)
+      console.log(`${playerOne.name} does not have enough cards to go to War. ${playerTwo.name} takes ${playerOne.name}'s final ${playerOneDeck.length + 1} cards.`)
+      playerTwoDeck.push(...playerOneDeck)
+      playerOneDeck = []
+      return
 
     } else if (playerTwoDeck.length < 4) {
-      console.log(`${playerTwo.name} does not have enough cards to go to War. ${playerOne.name} wins the game!`)
+      console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. WAR!!!`)
+      console.log(`${playerTwo.name} does not have enough cards to go to War. ${playerOne.name} takes ${playerTwo.name}'s final ${playerTwoDeck.length + 1} cards.`)
+      playerOneDeck.push(...playerTwoDeck)
+      playerTwoDeck = []
+      return
 
     } else {
       console.log(`${playerOne.name} played ${playerOneCard[0].rank} of ${playerOneCard[0].suit} and ${playerTwo.name} played ${playerTwoCard[0].rank} of ${playerTwoCard[0].suit}. WAR!!!`)
-      pile.push(playerOneDeck.splice(0, 3)); pile.push(playerTwoDeck.splice(0, 3))
-      pile.push(playerOneCard); pile.push(playerTwoCard)
+
+      pileOne.push(playerOneDeck.splice(0, 3)); pileTwo.push(playerTwoDeck.splice(0, 3))
+
+      combinedPile.push(...pileOne[0]); combinedPile.push(...pileTwo[0])
+      pileOne = []; pileTwo = []
+
+      combinedPile.push(...playerOneCard); combinedPile.push(...playerTwoCard)
+      playerOneCard = []; playerTwoCard = []
+
       playerOneCard = playerOneDeck.splice(0, 1); playerTwoCard = playerTwoDeck.splice(0, 1)
-      console.log(pile)
-      console.log(playerOneCard, playerTwoCard)
 
       if (playerOneCard[0].score > playerTwoCard[0].score) {
+        playerOneDeck.push(...combinedPile)
+        combinedPile = []
         playerOneWins()
-        playerOneDeck.push(...pile[0]); playerOneDeck.push(...pile[1]); playerOneDeck.push(...pile[2])
-        console.log(playerOneDeck)
-        console.log(playerTwoDeck)
 
       } else if (playerTwoCard[0].score > playerOneCard[0].score) {
+        playerTwoDeck.push(...combinedPile)
+        combinedPile = []
         playerTwoWins()
-        playerTwoDeck.push(...pile[0]); playerTwoDeck.push(...pile[1]); playerTwoDeck.push(pile[2])
-        console.log(playerOneDeck)
-        console.log(playerTwoDeck)
 
-      } else while (playerOneCard[0].score !== undefined && playerOneCard[0].score === playerTwoCard[0].score) {
+      } else while (playerOneCard.length !== 0 && playerOneCard[0].score === playerTwoCard[0].score) {
         war()
       }
     }
   }
-  // while (playerOneDeck.length > 0 && playerTwoDeck.length > 0) {
-  playerOneCard = playerOneDeck.splice(0, 1); playerTwoCard = playerTwoDeck.splice(0, 1)
+  while (playerOneDeck.length > 0 && playerTwoDeck.length > 0) {
+    playerOneCard = playerOneDeck.splice(0, 1); playerTwoCard = playerTwoDeck.splice(0, 1)
 
-  if (playerOneCard[0].score > playerTwoCard[0].score) {
-    playerOneWins()
+    if (playerOneCard[0].score > playerTwoCard[0].score) {
+      playerOneWins()
 
-  } else if (playerTwoCard[0].score > playerOneCard[0].score) {
-    playerTwoWins()
+    } else if (playerTwoCard[0].score > playerOneCard[0].score) {
+      playerTwoWins()
 
-  } else if (playerOneCard[0].score !== undefined && playerOneCard[0].score === playerTwoCard[0].score) {
-    war()
+    } else if (playerOneCard[0].score !== undefined && playerOneCard[0].score === playerTwoCard[0].score) {
+      war()
+    }
+  } if (playerOneDeck.length === 0) {
+    console.log(`${playerOne.name} has ${playerOneDeck.length} cards left. ${playerTwo.name} wins the game!`)
+
+  } else if (playerTwoDeck.length === 0) {
+    console.log(`${playerTwo.name} has ${playerTwoDeck.length} cards left. ${playerOne.name} wins the game!`)
   }
 }
-// if (playerOneDeck.length === 0) {
-//   console.log(`${playerOne.name} has no more cards. ${playerTwo.name} wins the game!`)
-
-// } else if (playerTwoDeck.length === 0) {
-//   console.log(`${playerTwo.name} has no more cards. ${playerOne.name} wins the game!`)
-// }
-// }
-gameOfWar()
-
+// gameOfWar()
